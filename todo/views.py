3bf65ab6d08779e74d7b2cc855eb05e57e48549f
brian-hornbrook <brian_hornbrook@outlook.com>
2022-09-15 14:40:09 -0500

    added create a user functionality

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
+
 
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'signupuser.html', {'form': UserCreationForm()})
-    # else:
-        # User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return render(request, 'todos.html')
+            except IntegrityError:
+                print("already exists")
+                return render(request, 'signupuser.html',
+                {'form': UserCreationForm(),
+                'errors': "that username or password was already taken"})
+        else:
+            return render(request, 'signupuser.html',
+                          {'form': UserCreationForm(),
+                          'errors': "that username or password was alredy taken"})
+
+
+def todos(request):
+    return render(request, 'todos.html')
